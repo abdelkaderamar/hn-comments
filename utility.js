cb9c47e935b9ec7c8993fdbb46079e0730031621
Abdelkader Amar <abdelkader.amar@gmail.com>
2023-02-22 19:01:54 +0100

    Refactoring: moving utility functions to utility.js

--- utility.js
+++ utility.js
@@ -1,5 +1,60 @@
-function foo() {
-  console.log("foo");
+function cleanLinksText(element)
+{
+  a_elts = element.querySelectorAll("a");
+  for (a of a_elts) {
+    console.log(a.getAttribute("href"));
+    a.innerText = " " + a.getAttribute("href") + " ";
+  }
+  console.log(element)
+}
+
+function stripHTML(html)
+{
+   let tmp = document.createElement("div");
+   tmp.innerHTML = html;
+   cleanLinksText(tmp);
+   tmp.innerHTML = tmp.innerHTML.replace('<p>', '\n</p>');
+   return  tmp.textContent || tmp.innerText || "";
+}
+
+function getStoryId(url) 
+{
+  console.log("Get the story id of url ", url);
+  storyId = url.replace(
+    "https://news.ycombinator.com/item?id=",
+    ""
+  );
+  if (storyId === url) {
+    storyId = "all";
+  }
+  console.log("storyId = ", storyId);
+  return  storyId;
+}
+
+function getSelectionHTML() {
+  var userSelection;
+  if (window.getSelection) {
+    // W3C Ranges
+    userSelection = window.getSelection();
+    // Get the range:
+    if (userSelection.getRangeAt) var range = userSelection.getRangeAt(0);
+    else {
+      var range = document.createRange();
+      range.setStart(userSelection.anchorNode, userSelection.anchorOffset);
+      range.setEnd(userSelection.focusNode, userSelection.focusOffset);
+    }
+    // And the HTML:
+    var clonedSelection = range.cloneContents();
+    var div = document.createElement("div");
+    div.appendChild(clonedSelection);
+    return div.innerHTML;
+  } else if (document.selection) {
+    // Explorer selection, return the HTML
+    userSelection = document.selection.createRange();
+    return userSelection.htmlText;
+  } else {
+    return "";
+  }
 }
 
 function exportStoryCommentsToText(storyId) {
